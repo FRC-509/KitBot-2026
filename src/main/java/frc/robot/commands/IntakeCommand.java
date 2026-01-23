3e417a004ac7039b40f2d633a090779c8585f762
shonk4 <9964449@bedfordnhk12.net>
2026-01-23 20:50:15 +0000

    Intake and Shooter Commands fixes

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -6,21 +6,21 @@ package frc.robot.commands;
 
 import frc.robot.subsystems.Subsystems;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake;
 
 /** An example command that uses an example subsystem. */
-public class Commands extends Command {
+public class IntakeCommand extends Command {
   @SuppressWarnings("PMD.UnusedPrivateField")
-  private final Subsystems m_subsystem;
+  private final Intake intakeN;
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public Commands(Subsystems subsystem) {
-    m_subsystem = subsystem;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+  public IntakeCommand(Intake intake) {
+    intakeN = intake;
+    addRequirements(intake);
   }
 
   // Called when the command is initially scheduled.
@@ -29,7 +29,10 @@ public class Commands extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    //maybe -0.75)
+    intakeN.intaker();
+  }
 
   // Called once the command ends or is interrupted.
   @Override
@@ -38,6 +41,6 @@ public class Commands extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return true;
   }
 }
--- src/main/java/frc/robot/commands/ShooterCommand.java
+++ src/main/java/frc/robot/commands/ShooterCommand.java
@@ -5,39 +5,40 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.Subsystems;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Shooter;
 
 /** An example command that uses an example subsystem. */
-public class Commands extends Command {
-  @SuppressWarnings("PMD.UnusedPrivateField")
-  private final Subsystems m_subsystem;
+public class ShooterCommand extends Command {
+  private final Shooter shooterN;
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public Commands(Subsystems subsystem) {
-    m_subsystem = subsystem;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+  public ShooterCommand(Shooter shooter) {
+    shooterN = shooter;
+
+    addRequirements(shooter.);
   }
 
-  // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    // maybe -0.75
+    shooterN.shoot();
+  }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {}
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return true;
   }
 }
